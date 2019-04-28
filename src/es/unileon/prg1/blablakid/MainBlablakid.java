23d7e2e2c7e8c7ed40ed1f1344ccc23c067eb86b
Marcos Ferreras Rodriguez <mferrr03@estudiantes.unileon.es>
2019-04-28 14:08:33 +0200

    Añadido manejo de excepciones. Código para pruebas

--- src/es/unileon/prg1/blablakid/MainBlablakid.java
+++ src/es/unileon/prg1/blablakid/MainBlablakid.java
@@ -3,6 +3,17 @@ package es.unileon.prg1.blablakid;
 public class MainBlablakid{
 
 	public static void main(String[] args) {
-	System.out.println("Hola mundo");
+		Blablakid blablakid;
+		try {
+			if (args.length == 1) {
+				int maxNumOfKids = Integer.parseInt(args[0]);
+				blablakid = new Blablakid(maxNumOfKids);
+			} else {
+				throw new BlaException("Error: Indique un único parámetro en la ejecución del programa. java –cp classes es.unileon.prg1.blablakid.MainBlablakid maxKids");
+			}
+			blablakid.addKid("Aurelio");
+		} catch (BlaException e) {
+		System.out.println(e.getMessage());
+		}
 	}
 }
