e38f4e88413425fcc1a3b6183b369707d982f261
mario <malvai00@estudiantes.unileon.es>
2019-04-28 19:44:29 +0200

    codigo bruto de la clase padre y padres completado

--- src/es/unileon/prg1/blablakid/Parent.java
+++ src/es/unileon/prg1/blablakid/Parent.java
@@ -1,1 +1,67 @@
 package es.unileon.prg1.blablakid;
+public class Parent{
+	private String name;
+	//private Week Week;
+	private String hijo[];
+	private int n_hijos;
+	private int n_viajes;
+	
+	protected Parent(String name,int n_hijos,int n_viajes, String hijo[]) throws BlaException {
+		this.name=name;
+		if (isCorrectNumberHijos(n_hijos)) {
+			for (int i=0;i<n_hijos;i++) {
+				this.hijo[i]=hijo[i];
+			}
+		}
+		if (isCorrectNumberViajes(n_viajes)) {
+			this.n_viajes=n_viajes;
+		}
+	}
+	
+	protected boolean isCorrectNumberHijos(int number) throws BlaException {
+		boolean salida=false;
+		if (number>0) salida=true;
+		else throw new BlaException("El numero de hijos ha de ser mayor de 0.");
+		return salida;
+	}
+	
+	protected boolean isCorrectNumberViajes(int number) throws BlaException {
+		boolean salida=false;
+		if (number>0) salida=true;
+		else throw new BlaException("El numero de viajes ha de ser mayor de 0.");
+		return salida;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHijo(int number) {
+		return hijo[number];
+	}
+
+	public void setHijo(String hijo, int numero) {
+		this.hijo[numero] = hijo;
+	}
+
+	public int getN_hijos() {
+		return n_hijos;
+	}
+
+	public void setN_hijos(int n_hijos) {
+		this.n_hijos = n_hijos;
+	}
+
+	public int getN_viajes() {
+		return n_viajes;
+	}
+
+	public void setN_viajes(int n_viajes) {
+		this.n_viajes = n_viajes;
+	}
+	
+	
+}
--- src/es/unileon/prg1/blablakid/Parents.java
+++ src/es/unileon/prg1/blablakid/Parents.java
@@ -1,1 +1,63 @@
 package es.unileon.prg1.blablakid;
+
+public class Parents {
+	int maxNumOfParents;
+	private Parent parent[];
+	
+	protected Parents(int numParents) throws BlaException {
+		if (isCorrectNumber(numParents)) parent = new Parent[numParents];
+	}
+	
+	protected boolean isCorrectNumber(int numParents) throws BlaException {
+		boolean salida;
+		if (numParents>=0) salida=true;
+		else {
+			salida=false;
+			throw new BlaException ("El numero introducido mayor que 0.");
+		}
+		return salida;
+	}
+	
+	public boolean checkParentExists(String name) {
+			boolean salida=false;
+		int i=0;
+		do {
+			if (parent[i].getName()==name) salida=true;
+			i++;
+		}while ((salida==false) && (parent[i]!=null) && (i<this.maxNumOfParents));
+		return salida;
+	}
+	
+	public boolean addParent(Parent parent) {
+		int i=0;
+		boolean salida=true;
+		while ( this.parent[i] != null) i++;
+		if (i>maxNumOfParents) salida=false;
+		else this.parent[i]=parent;
+		return salida;
+	}
+	
+	public boolean deleateParent(Parent parent) throws BlaException{
+		boolean salida=false;
+		int i=0;
+		if (checkParentExists(parent.getName())==true) {
+			while(this.parent[i].getName()!=parent.getName()) i++;
+			this.parent[i]=null;
+			reorganice(i);
+			salida=true;
+		} else throw new BlaException("Esa persona no existe");
+		return salida;
+	}
+	
+	private void reorganice(int number) {
+		int i=0;
+		while (this.parent[i]!=null) i++;
+		Parent aux;
+		for (int j=number;j<i;j++) {
+			aux=this.parent[j+1];
+			this.parent[j]=aux;
+		}
+		this.parent[i]=null;
+	}
+}
+	
