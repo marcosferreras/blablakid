a9bb7825af2048e46d5c258624766718d7a1d763
Marcos Ferreras Rodríguez <mferrr03@estudiantes.unileon.es>
2019-04-29 19:45:41 +0200

    Añadida interfaz de usuario

---
+++ src/es/unileon/prg1/blablakid/TextUI.java
@@ -0,0 +1,12 @@
+package es.unileon.prg1.blablakid;
+
+public class TextUI {
+	Blablakid blablakid;
+	protected TextUI (Blablakid blablakid) {
+		this.blablakid = blablakid;
+	}
+	
+	protected void start() {
+		//TODO Menú
+	}
+}
