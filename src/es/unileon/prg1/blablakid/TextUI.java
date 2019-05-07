7ae49199f3197c707beb4cc36f4c583ade7b63fe
mgarco07 <mgarco07@estudiantes.unileon.es>
2019-05-07 12:10:28 +0200

    avanzando textui

--- src/es/unileon/prg1/blablakid/TextUI.java
+++ src/es/unileon/prg1/blablakid/TextUI.java
@@ -37,23 +37,27 @@ public class TextUI {
 			System.out.println ("10 -Check status");
 
 			System.out.println ("0 - Exit");
+			number= teclado.readInteger();//recogemos ese valor en int numero	
+			selectOption(number);
+			} catch (BlaException e) {
+				System.out.println(e.getMessage());
+				}
+		}while (number != 0);
 
+	}
+	public void selectOption(int number)throws BlaException{
 
-				number= teclado.readInteger();//recogemos ese valor en int numero	
 			switch (number){
-				case 1:
-					System.out.println ("Name the kid to add:\n");
-					blablakid.addKid(new Kid(teclado.readString()));
+				case 1: askKid();
+					
 					break;
-				case 2:
-					System.out.println ("Name the kid to remove:\n");
-					blablakid.removeKid(teclado.readString());
+				case 2: removeKid();
 					break;
-				case 3:
+				case 3: askParent();
 					break;
-				case 4:
+				case 4: removeParent();
 					break;
-				case 5:
+				case 5: askActivity();
 					break;
 				case 6:
 					break;
@@ -72,12 +76,42 @@ public class TextUI {
 					System.out.println("Error: el número introducido no es correcto, introduzca un número del menú");
 
 			}
-		   } catch (BlaException e) {
-				System.out.println(e.getMessage());
-				}
-
-		}while (number != 0);
-
+	}
+	public void askKid(){
+		System.out.println ("Name the kid to add:\n");
+		blablakid.addKid(new Kid(teclado.readString()));//me crea un niño con el nombre introducido  (constructor de Kid)
 	}
 	
+	public void removeKid(){
+		System.out.println ("Name the kid to remove:\n");
+		blablakid.removeKid(teclado.readString());
+		System.out.println ("Remove kid error: Kid Daniel not found:\n");//Error
+	}
+	public void askParent(){ //pregunta por todos los datos del parent
+		System.out.println ("Name of the parent to add:");
+		//blablakid.addParent(new Parent(teclado.readString())); 
+	
+		System.out.println ("How many kids does Pedro have?:");
+		System.out.println ("How many rides can Pedro make per day?:");
+		System.out.println ("Who is"+????+"'s kid number"+??"?:");
+	}
+	
+	public void removeParent(){
+		System.out.println ("Name of the parent to remove:\n");
+	}
+
+	public void askActivity(){
+		System.out.println ("Name of the activity:");
+		System.out.println ("Where does the activity Baloncesto takes place?");
+		System.out.println ("Day of the week for the activity:\nInsert the number of the day of the week:\n0 -Monday / 1-Tuesday / 2 -Wednesday / 3 -Thursday / 4 -Friday");
+		switch(){
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+		}
+		System.out.println ("Name of the kid taking the activity:");
+		System.out.println ("When does the activity start? \n Insert hour:");
+		System.out.println ("Insert minute:");
 }
