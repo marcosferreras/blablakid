7571f349ac487f36ee7eaec408bada102b35763d
Marcos Ferreras Rodríguez <mferrr03@estudiantes.unileon.es>
2019-05-11 10:22:28 +0200

    Avanzando en el código

--- src/es/unileon/prg1/blablakid/Activity.java
+++ src/es/unileon/prg1/blablakid/Activity.java
@@ -1,17 +1,31 @@
 package es.unileon.prg1.blablakid;
 public class Activity{
-	int next, day;;
-	String name, place;
-	Time after;
-	Time before;
-	public Activity (String name, String place, int day, Time after, Time before) {
+	int next;
+	WeekDays day;
+	String name;
+	String place;
+	Time start;
+	Time end;
+	Ride before;
+	Ride after;
+	public Activity (String name, String place, WeekDays day, Time start, Time end) {
 		this.name = name;
 		this.place = place;
 		this.day = day;
-		this.after = after;
-		this.before = before;
+		this.start = start;
+		this.end = end;
 	}
 	public String getName(){
 		return this.name;
 	}
+	/*public void add(Ride ride) {
+		isValid(ride);
+	}
+	public boolean isValid(Ride ride) {
+		boolean valid = false;
+		if (ride.getTime().get ==  ) {
+			this.start = ride;
+		}
+		return valid;
+	}*/
 }
