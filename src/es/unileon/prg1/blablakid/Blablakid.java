5fe2a0a648ebd5de5576a351de53b12609cd57cb
Marcos García <mgarco07@estudiantes.unileon.es>
2019-05-14 12:45:15 +0200

    Nuevos métodos en TextUI y en Blablakid

--- src/es/unileon/prg1/blablakid/Blablakid.java
+++ src/es/unileon/prg1/blablakid/Blablakid.java
@@ -1,17 +1,25 @@
 package es.unileon.prg1.blablakid;
 public class Blablakid{
 	Kids kids;
+	Parents parents;
 	protected Blablakid(int maxNumOfKids) throws BlaException{
 		kids = new Kids(maxNumOfKids);
 	}
-	void addKid(Kid kid) throws BlaException {
+	void add(Kid kid) throws BlaException {
 		kids.add(kid);
 	}
+	public Kids getKids() {
+		return this.kids;
+	}
+	
 	void removeKid(String name) throws BlaException {
 		kids.remove(name);
 	}
-	void addActivity(Activity activity, String nameKid) throws BlaException {
+	void add(Activity activity, String nameKid) throws BlaException {
 		kids.search(nameKid).add(activity);
+	}
+	void add(Parent parent, Kids kids) throws BlaException{
+		parents.add(parent);
 	}
 	public String toString(){
 		return kids.toString();
