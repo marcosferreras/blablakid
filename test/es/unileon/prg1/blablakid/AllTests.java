9281984a3ca3f0a5ffd87dcee83afa5e0902dc80
Marcos Ferreras Rodríguez <mferrr03@estudiantes.unileon.es>
2019-05-20 21:50:12 +0200

    Funcionando ant con test

--- test/es/unileon/prg1/blablakid/AllTests.java
+++ test/es/unileon/prg1/blablakid/AllTests.java
@@ -1,1 +1,11 @@
 package es.unileon.prg1.blablakid;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ActivitiesTests.class, ActivityTests.class, BlablakidTests.class, ParentTest.class, DayTest.class, KidsTests.class, KidTests.class, ParentsTests.class, ParentTest.class, TimeTest.class })
+public class AllTests {
+
+}
--- test/es/unileon/prg1/blablakid/BlablakidTests.java
+++ test/es/unileon/prg1/blablakid/BlablakidTests.java
@@ -1,1 +1,4 @@
 package es.unileon.prg1.blablakid;
+import static org.junit.Assert.*;
+public class BlablakidTests {
+}
