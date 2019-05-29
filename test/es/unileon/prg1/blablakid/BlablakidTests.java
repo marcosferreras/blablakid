8148de65f6f918d122c372b127fb02fba677fd33
Marcos Ferreras Rodríguez <mferrr03@estudiantes.unileon.es>
2019-05-30 00:01:19 +0200

    Avance de test

--- test/es/unileon/prg1/blablakid/BlablakidTests.java
+++ test/es/unileon/prg1/blablakid/BlablakidTests.java
@@ -13,7 +13,7 @@ public class BlablakidTests {
 		Kids kids = new Kids(1);
 		kids.add(new Kid("Daniel"));
 		this.blablakid.add(new Kid("Daniel"));
-		this.blablakid.add(new Parent("Pedro",1,1),kids);
+		this.blablakid.add(new Parent("Pedro",2,1),kids);
 	}
 	@Test (expected = BlaException.class)
 	public void testNumOfKidsNegative() throws BlaException {
@@ -24,11 +24,16 @@ public class BlablakidTests {
 		this.blablakid.removeKid("Daniel"); 
 	}
 	@Test (expected = BlaException.class)
-	public void testAddActivityFalseKid() throws BlaException {
+	public void testRemoveFakeKid() throws BlaException {
+		this.blablakid.removeKid("Danie"); 
+	}
+	
+	@Test (expected = BlaException.class)
+	public void testAddActivityFakeKid() throws BlaException {
 		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Helena");
 	}
 	@Test 
-	public void testAddActivity() throws BlaException {
+	public void testAddActivity() throws BlaException { 
 		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Daniel");
 	}
 	@Test 
@@ -36,13 +41,31 @@ public class BlablakidTests {
 		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Daniel");
 		this.blablakid.removeActivity("Daniel", "Baloncesto", WeekDays.MONDAY);
 	}
+	@Test (expected = BlaException.class)
+	public void testRemoveActivityFakeKid() throws BlaException {
+		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Daniel");
+		this.blablakid.removeActivity("Danie", "Baloncesto", WeekDays.MONDAY);
+	} 
+	@Test 
+	public void testRemoveActivityWithRides() throws BlaException {
+		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Daniel");
+		this.blablakid.addRide("Pedro", "Baloncesto", "Daniel", new Ride(new Time(17,00),new Time(18,00), "Casa", "Palomera"), WeekDays.MONDAY);
+		this.blablakid.addRide("Pedro", "Baloncesto", "Daniel", new Ride(new Time(21,00),new Time(22,00), "Palomera", "Casa"), WeekDays.MONDAY);
+		this.blablakid.removeActivity("Daniel", "Baloncesto", WeekDays.MONDAY);
+	}
 	@Test 
 	public void testAddRide() throws BlaException {
 		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Daniel");
 		this.blablakid.addRide("Pedro", "Baloncesto", "Daniel", new Ride(new Time(17,00),new Time(18,00), "Casa", "Palomera"), WeekDays.MONDAY);
 	}
+	@Test 
+	public void testRemoveRide() throws BlaException {
+		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Daniel");
+		this.blablakid.addRide("Pedro", "Baloncesto", "Daniel", new Ride(new Time(17,00),new Time(18,00), "Casa", "Palomera"), WeekDays.MONDAY);
+		this.blablakid.removeRide("Pedro", WeekDays.MONDAY, "Casa", "Palomera");
+	}
 	@Test (expected = BlaException.class)
-	public void testAddRideIncorrectKid() throws BlaException {
+	public void testAddRideFakeKid() throws BlaException {
 		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Daniel");
 		this.blablakid.addRide("Pedro", "Baloncesto", "Manuel", new Ride(new Time(17,00),new Time(18,00), "Casa", "Palomera"), WeekDays.MONDAY);
 	}
