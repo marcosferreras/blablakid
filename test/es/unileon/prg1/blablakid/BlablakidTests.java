e2dc3b8cfba9043b1d054590f79315964990a502
Marcos Ferreras Rodríguez <mferrr03@estudiantes.unileon.es>
2019-05-30 08:57:41 +0200

    Añadidos mas test

--- test/es/unileon/prg1/blablakid/BlablakidTests.java
+++ test/es/unileon/prg1/blablakid/BlablakidTests.java
@@ -52,17 +52,71 @@ public class BlablakidTests {
 		this.blablakid.addRide("Pedro", "Baloncesto", "Daniel", new Ride(new Time(17,00),new Time(18,00), "Casa", "Palomera"), WeekDays.MONDAY);
 		this.blablakid.addRide("Pedro", "Baloncesto", "Daniel", new Ride(new Time(21,00),new Time(22,00), "Palomera", "Casa"), WeekDays.MONDAY);
 		this.blablakid.removeActivity("Daniel", "Baloncesto", WeekDays.MONDAY); 
+		assertEquals("////////////////////////\n" + 
+				"\n" + 
+				"KIDS:\n" + 
+				"\n" + 
+				"****** Daniel ******\n" + 
+				"PARENTS:\n" + 
+				"##### Pedro #####\n" + 
+				"Kids:\n" + 
+				"\n" + 
+				"****** Daniel ******\n" + 
+				"\n" + 
+				"RIDES:\n" + 
+				"MONDAY rides:\n" + 
+				"Casa > Palomera : 17:0 / 18:0\n" + 
+				"\n" + 
+				"////////////////////////", this.blablakid.toString());
 	}
 	@Test 
 	public void testAddRide() throws BlaException {
 		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Daniel");
 		this.blablakid.addRide("Pedro", "Baloncesto", "Daniel", new Ride(new Time(17,00),new Time(18,00), "Casa", "Palomera"), WeekDays.MONDAY);
+		assertEquals("////////////////////////\n" + 
+				"\n" + 
+				"KIDS:\n" + 
+				"\n" + 
+				"****** Daniel ******\n" + 
+				"##### Activity 1 #####\n" + 
+				"Baloncesto(Palomera - MONDAY)18:0>20:0\n" + 
+				"Casa > Palomera : 17:0 / 18:0\n" + 
+				"No ride after Baloncesto assigned\n" + 
+				"PARENTS:\n" + 
+				"##### Pedro #####\n" + 
+				"Kids:\n" + 
+				"\n" + 
+				"****** Daniel ******\n" + 
+				"\n" + 
+				"RIDES:\n" + 
+				"MONDAY rides:\n" + 
+				"Casa > Palomera : 17:0 / 18:0\n" + 
+				"\n" + 
+				"////////////////////////", this.blablakid.toString());
 	}
 	@Test 
 	public void testRemoveRide() throws BlaException {
-		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,00)), "Daniel");
-		this.blablakid.addRide("Pedro", "Baloncesto", "Daniel", new Ride(new Time(17,00),new Time(18,00), "Casa", "Palomera"), WeekDays.MONDAY);
+		this.blablakid.add(new Activity("Baloncesto","Palomera",WeekDays.MONDAY,new Time(18,00),new Time(20,30)), "Daniel");
+		this.blablakid.addRide("Pedro", "Baloncesto", "Daniel", new Ride(new Time(17,00),new Time(17,30), "Casa", "Palomera"), WeekDays.MONDAY);
 		this.blablakid.removeRide("Pedro", WeekDays.MONDAY, "Casa", "Palomera");
+		assertEquals("////////////////////////\n" + 
+				"\n" + 
+				"KIDS:\n" + 
+				"\n" + 
+				"****** Daniel ******\n" + 
+				"##### Activity 1 #####\n" + 
+				"Baloncesto(Palomera - MONDAY)18:0>20:30\n" + 
+				"No ride before Baloncesto assigned\n" + 
+				"No ride after Baloncesto assigned\n" + 
+				"PARENTS:\n" + 
+				"##### Pedro #####\n" + 
+				"Kids:\n" + 
+				"\n" + 
+				"****** Daniel ******\n" + 
+				"\n" + 
+				"RIDES:\n" + 
+				"\n" + 
+				"////////////////////////", this.blablakid.toString());
 	}
 	@Test (expected = BlaException.class)
 	public void testAddRideFakeKid() throws BlaException {
